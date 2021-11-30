da28698d6362ef5419acae509bfc470bfd2e24b3
George Tsiftsis <gtsiff@gmail.com>
2021-11-30 23:50:10 +0200

    Main page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,42 @@
 import Head from "next/head";
+import SimpleMap from "../components/maps/SimpleMap";
+import Draseis from "../components/Draseis";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="py-2 flex flex-col items-center justify-center">
       <Head>
         <title>Theranotics MBG Duth </title>
-        <link rel="icon" href="/favicon.ico" /> 
-
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-
-     
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <h1> NEW </h1>
+      <Draseis />
+      <SimpleMap />
     </div>
   );
 }
