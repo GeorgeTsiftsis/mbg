86ef150d90389aaca7b13b206e45859210b73acf
George Tsiftsis <gtsiff@gmail.com>
2021-12-06 23:02:58 +0200

    Epikentro ereunas

---
+++ components/EpikentroEreunas/EpikentroChild.js
@@ -0,0 +1,50 @@
+function EpikentroChild({ title, description, letter, image }) {
+  return (
+    <div className=" flex flex-col  drop-shadow-2xl md:w-80 justify-center items-center  font-manrope  m-4  ">
+      <img className=" w-full" alt="Biomarkers" src={image} />
+      <div className="flex flex-col bg-myepikentro hover:bg-blue-400  justify-center items-center ">
+        <div className="self-center  ">
+          <div className="relative">
+            <img src="./hexagon-128.png" />
+            <h1
+              style={{
+                position: "absolute",
+                right: "35px",
+                top: "42px",
+                fontSize: "95px",
+              }}
+              className="text-4xl text-black text-center"
+            >
+              {letter}
+            </h1>
+          </div>
+        </div>
+        <h1 className="  text-center text-3xl  pb-2 text-white">{title}</h1>
+
+        <div>
+          <div className="flex  flex-col justify-items justify-center ">
+            <p
+              style={{
+                textAlign: "justify",
+                // wordSpacing: "-2px",
+                textJustify: "auto",
+                // text-align: justify;
+                // textAlign: "center",
+                // textJustify: "inter-word",
+                // whiteSpace: "pre-line",
+                // whiteSpace: "pre-line",
+
+                // textAlignLast: "left",
+              }}
+              className=" place-self-start p-4 text-base text-white h-40"
+            >
+              {description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EpikentroChild;
---
+++ components/EpikentroEreunas/index.js
@@ -0,0 +1,52 @@
+import EpikentroChild from "./EpikentroChild";
+function EpikentroEreunas() {
+  return (
+    <div className="flex flex-col   items-center justify-center max-w-7xl">
+      <h1 className=" text-blue-400 text-2xl text-center pb-4">
+        Επίκεντρο της έρευνας μας είναι
+      </h1>
+      <div className="flex flex-wrap justify-center self-center flex-col md:flex-row  w-12/12">
+        <EpikentroChild
+          title="Biomarkers"
+          description="Η μελέτη της γονιδιακής έκφρασης με σκοπό την ανακάλυψη βιοδεικτών
+            και την αξιοποίησή τους ως μοριακούς διαγνωστικούς δείκτες ή ως
+            θεραπευτικούς στόχους."
+          letter="B"
+          image="./item1.png"
+        />
+        <EpikentroChild
+          title="Recombinant DNA"
+          description="H εφαρμογή της τεχνολογίας ανασυνδυασμένου DNA για την παραγωγή συνθετικών πολυπεπτιδίων καθώς η μελέτη της σχέσης μοριακής δομής και λειτουργίας."
+          letter="R"
+          image="./item2.png"
+        />
+        <EpikentroChild
+          title="Protein Purification"
+          description="H απομόνωση φυσικών πρωτεϊνών υψηλής προστιθέμενης αξίας με σημαντικές βιοτεχνολογικές εφαρμογές."
+          letter="P"
+          image="./item3.png"
+        />
+        <EpikentroChild
+          title="Molecular Agrobiology"
+          description="Η χρήση των μεθόδων της Μοριακής Βιολογίας στην Αγροβιολογία με σκοπό τον προσδιορισμό τρόπων αύξησης των αποδόσεων των καλλιεργειών."
+          letter="A"
+          image="./item4.png"
+        />
+        <EpikentroChild
+          title="Exosomes"
+          description="Μελέτη χρήσης εξωσωμάτων ως βιοδείκτες εξέλιξης παθολογικών καταστάσεων και ως θεραπευτική επιλογή. "
+          letter="E"
+          image="./item5.png"
+        />
+        <EpikentroChild
+          title="Probiotics"
+          description="H μελέτη των μοριακών μηχανισμών δράσης των προβιοτικών μικροοργανισμών με βιοιατρικές εφαρμογές."
+          letter="P"
+          image="./item6.png"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default EpikentroEreunas;
