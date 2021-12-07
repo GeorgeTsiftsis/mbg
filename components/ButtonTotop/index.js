fa96510d8fcef4fcfce131219e474ab0d24a6229
George Tsiftsis <gtsiff@gmail.com>
2021-12-07 03:54:25 +0200

    improve button to top

--- components/ButtonTotop/index.js
+++ components/ButtonTotop/index.js
@@ -6,10 +6,10 @@ function BottomToTop() {
     scroll.scrollToTop();
   };
   return (
-    <div style={{ top: "100%" }} className="sticky w-full z-10 ">
+    <div style={{ top: "100%" }} className="sticky w-full z-10  ">
       <div style={{ position: "absolute", bottom: "0", right: "0" }}>
         <button
-          className="bg-myfooter opacity-30 border-black border-2 "
+          className="bg-myfooter m-2 opacity-30 border-black border-2 "
           onClick={scrollToTop}
         >
           <AiOutlineArrowUp color="white" fontSize="1.9rem" />
