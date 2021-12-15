b4774ce72862b42dccaa879e3af695fe64128012
George Tsiftsis <gtsiff@gmail.com>
2021-12-16 00:16:25 +0200

    Create Graduates Component

---
+++ components/MembersOfSandaltzopoulosGroup/DummyData.js
@@ -0,0 +1,40 @@
+export const data1row = [
+  {
+    name: "Αντώνης Γιαννακάκης",
+    url: "https://www.linkedin.com/in/antonis-giannakakis-13b8115/?originalSubdomain=sg",
+  },
+  {
+    name: "Ευριπίδης Λανίτης",
+    url: "https://www.linkedin.com/in/evripidis-lanitis-4a8064113/",
+  },
+  {
+    name: "Denarda Dangaj",
+    url: "https://www.linkedin.com/in/denarda-dangaj-8b36a288/",
+  },
+];
+export const data2row = [
+  {
+    name: "Θανάσης Καραπέτσας",
+    url: "https://www.linkedin.com/in/athanasios-karapetsas-923b21105/",
+  },
+  {
+    name: "Ευθυμία Ι. Ματθαίου",
+    url: "https://www.linkedin.com/in/efthymia-iliana-matthaiou-699b4a10a/",
+  },
+  {
+    name: "Τοκαμάνη Μαρία",
+    url: "https://www.linkedin.com/in/maria-tokamani-8790ba67/",
+  },
+];
+
+export const data3row = [
+  { name: "Κατερίνα Σεμιλιέτωφ", url: "linked" },
+  {
+    name: "Βαγγέλης Στεφανίδης",
+    url: "https://www.linkedin.com/in/evangelos-stefanidis-5574a4120/",
+  },
+  {
+    name: "Κατερίνα Γεμενέτζη",
+    url: " https://www.linkedin.com/in/katerina-gemenetzi-39115b113/?originalSubdomain=gr",
+  },
+];
--- components/MembersOfSandaltzopoulosGroup/Graduates.js
+++ components/MembersOfSandaltzopoulosGroup/Graduates.js
@@ -1,28 +1,64 @@
+import { AiFillLinkedin } from "react-icons/ai";
+import { data1row, data2row, data3row } from "./DummyData";
 function Graduates() {
   return (
-    <div className="items-center justify-items flex flex-col">
+    <div className="items-center justify-items p-2 flex flex-col">
       <h1 className="text-center font-manrope text-5xl underline py-8">
         Απόφοιτοι Εργαστηρίου
       </h1>
-      <div>
-        <ul className="flex flex-col   items-center justify-center  p-8">
+      <img alt="graduates" src="./academics.png" className="rounded-lg" />
+      <h1 className="text-center font-manrope text-3xl underline py-6">
+        Απόφοιτοι Διδάκτορες
+      </h1>
+      <div className="pb-4 lg:w-full w-3/4 flex flex-col w-full items-center justify-center ">
+        <ul className=" flex flex-wrap rounded-lg text-center underline  text-2xl border-2 flex-row justify-around items-around  ">
           <div>
-            <li>Αντώνης Γιαννακάκης</li>
-            <li>Ευριπίδης Λανίτης</li>
-            <li>Denarda Dangaj</li>
+            {data1row.map((element, id) => (
+              <a
+                key={id}
+                className="flex items-center text-center p-2 hover:text-blue-400  "
+                href={element.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillLinkedin size="2rem" className="text-mygraduatesblue " />
+                <li>{element.name}</li>
+              </a>
+            ))}
           </div>
-          <div className="m-4">
-            <li>Θανάσης Καραπέτσας</li>
-            <li>Ευθυμία Ι. Ματθαίου</li>
-            <li>Τοκαμάνη Μαρία</li>
-          </div>
-          <div>
-            <li>Κατερίνα Σεμιλιέτωφ</li>
-            <li>Βαγγέλης Στεφανίδης </li>
-            <li>Κατερίνα Γεμενέτζη</li>
-          </div>
+          <ul>
+            {data2row.map((element, id) => (
+              <a
+                key={id}
+                className="flex items-center p-2 hover:text-blue-400  "
+                href={element.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillLinkedin size="2rem" className="text-mygraduatesblue" />
+                <li>{element.name}</li>
+              </a>
+            ))}
+          </ul>
+          <ul>
+            {data3row.map((element, id) => (
+              <a
+                key={id}
+                className="flex items-center p-2 hover:text-blue-400  "
+                href={element.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillLinkedin size="2rem" className="text-mygraduatesblue" />
+                <li>{element.name}</li>
+              </a>
+            ))}
+          </ul>
         </ul>
       </div>
+      <h1 className="text-center font-manrope text-3xl underline py-6">
+        Απόφοιτοι Μεταπτυχιακοί
+      </h1>
     </div>
   );
 }
