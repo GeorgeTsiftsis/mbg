931df0e0ecdac9aa5bc3575731b9eb7d55410b88
George Tsiftsis <gtsiff@gmail.com>
2021-12-22 19:02:43 +0200

    Secure my firebase

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,13 +3,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 var config = {
-  apiKey: "AIzaSyCbFxoVdXc8_0lgP2nBJPZUNTeilqgUVxE",
-  authDomain: "mbgduth-58b1b.firebaseapp.com",
-  projectId: "mbgduth-58b1b",
-  storageBucket: "mbgduth-58b1b.appspot.com",
-  messagingSenderId: "503272333964",
-  appId: "1:503272333964:web:b227a2b2621bec813111a9",
-  measurementId: "G-Y5XP02H1DN",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
