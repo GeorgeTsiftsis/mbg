9e8e3d555abe48920fc62bab9cffb146574fcd4d
George Tsiftsis <gtsiff@gmail.com>
2021-12-23 15:40:18 +0200

    Initialize archive lab page

--- pages/LabArchive.js
+++ pages/LabArchive.js
@@ -1,7 +1,11 @@
 function LabArchive() {
   return (
-    <div>
-      <h1>Αρχειο will be fixed</h1>
+    <div className="flex flex-col p-4 max-4-xl items-center justify-center font-manrope">
+      <h1 className="text-5xl underline  text-center py-4 ">Αρχείο</h1>
+      <p className="py-2">
+        Παρακάτω μπορείτε να βρείτε τις πτυχιακές διατριβές των εργαστηρίων μας
+        ταξινομημενες ανα χρονολογία
+      </p>
     </div>
   );
 }
