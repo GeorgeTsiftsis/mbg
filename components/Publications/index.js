d0e13fbeb592ec3a15f23c62bb8e17008f8cc025
George Tsiftsis <gtsiff@gmail.com>
2021-12-23 15:39:44 +0200

    Add SEARCH BAR

--- components/Publications/index.js
+++ components/Publications/index.js
@@ -1,4 +1,8 @@
+import autoprefixer from "autoprefixer";
+import { useState } from "react";
 function Publications() {
+  const [query, setQuery] = useState("");
+
   const publications = [
     {
       name: "Gemenetzi, Katerina, et al. 'Higher-order immunoglobulin repertoire restrictions in CLL: the illustrative case of stereotyped subsets 2 and 169'. Blood 137.14 (2021): 1895-1904.",
@@ -37,16 +41,37 @@ function Publications() {
     },
   ];
 
-  return publications.map((element, id) => (
-    <a
-      key={id}
-      href={element.link}
-      target="_blank"
-      rel="noopener noreferrer"
-      className={element.link ? "hover:text-blue-400" : ""}
-    >
-      <p className="max-w-4xl  text-lg text-left  py-1  ">{element.name}</p>
-    </a>
-  ));
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center">
+        <input
+          className="py-2 m-2 text-center  border-2 rounded-lg border-gray-400"
+          placeholder="Αναζήτηση"
+          onChange={(event) => setQuery(event.target.value)}
+        />
+      </div>
+      {publications
+        .filter((element) => {
+          if (query === "") {
+            return element;
+          } else if (element.name.toLowerCase().includes(query.toLowerCase())) {
+            return element;
+          }
+        })
+        .map((element, id) => (
+          <a
+            key={id}
+            href={element.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={element.link ? "hover:text-blue-400" : ""}
+          >
+            <p className="max-w-4xl  text-lg text-left  py-1  ">
+              {element.name}
+            </p>
+          </a>
+        ))}
+    </div>
+  );
 }
 export default Publications;
