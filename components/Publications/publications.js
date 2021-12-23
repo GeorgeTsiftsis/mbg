b866fa302151b05c91a33df668043b346dec665a
George Tsiftsis <gtsiff@gmail.com>
2021-12-23 16:25:41 +0200

    Finalize Publications

---
+++ components/Publications/publications.js
@@ -0,0 +1,256 @@
+export const publications = [
+  {
+    name: "Gemenetzi, Katerina, et al. 'Higher-order immunoglobulin repertoire restrictions in CLL: the illustrative case of stereotyped subsets 2 and 169'. Blood 137.14 (2021): 1895-1904.",
+    link: "https://www.sciencedirect.com/science/article/abs/pii/S0006497121007771",
+  },
+  {
+    name: "Agathangelidis, Andreas, et al. 'Higher-order connections between stereotyped subsets: implications for improved patient classification in CLL. Blood', The Journal of the American Society of Hematology 137.10 (2021): 1365-1376.",
+    link: "https://ashpublications.org/blood/article/137/10/1365/463992/Higher-order-connections-between-stereotyped",
+  },
+  {
+    name: "Balaska, Konstantina, et al. 'Genetic diversity and spatial structure of the imperiled European population of Malus trilobata in Greece.' Genetika 53.1 (2021): 103-119",
+    link: "http://www.doiserbia.nb.rs/Article.aspx?id=0534-00122101103B#.YbkBshZBx0w",
+  },
+  {
+    name: "Kotouza, Maria Th, et al. 'TRIP-T cell receptor/immunoglobulin profiler.' BMC bioinformatics 21.1 (2020): 1-21.",
+    link: "https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-020-03669-1",
+  },
+  {
+    name: "Sinapidou, Evaggelia, et al. 'Plant Yield Efficiency by Homeostasis as Selection Tool at Ultra-Low Density. A Comparative Study with Common Stability Measures in Maize.' Agronomy 10.8 (2020): 1203.",
+    link: "https://www.mdpi.com/2073-4395/10/8/1203",
+  },
+  {
+    name: "Mylonas, Ioannis, et al. 'Improved plant yield efficiency alleviates the erratic optimum density in maize.' Agronomy Journal 112.3 (2020): 1690-1701.",
+    link: "https://acsess.onlinelibrary.wiley.com/doi/abs/10.1002/agj2.20187",
+  },
+  {
+    name: "Gemenetzi, Katerina, et al. 'Truly unmutated IGHV-IGHD-IGHJ gene rearrangements in CLL: do they really exist?.' LEUKEMIA & LYMPHOMA. Vol. 61. 2-4 PARK SQUARE, MILTON PARK, ABINGDON OR14 4RN, OXON, ENGLAND: TAYLOR & FRANCIS LTD, 2020.",
+  },
+  {
+    name: "Gemenetzi, Katerina, et al. 'Higher Order Restrictions of the Immunoglobulin Repertoire in CLL: The Illustrative Case of Stereotyped Subsets# 2 and# 169.' (2019): 5453-5453.",
+    link: "https://ashpublications.org/blood/article/134/Supplement_1/5453/425281/Higher-Order-Restrictions-of-the-Immunoglobulin",
+  },
+  {
+    name: "Gemenetzi, Katerina, et al. 'VH CDR3-focused somatic hypermutation in CLL IGHV-IGHD-IGHJ gene rearrangements with 100% IGHV germline identity.' (2019): 4277-4277.",
+    link: "https://ashpublications.org/blood/article/134/Supplement_1/4277/425643/VH-CDR3-Focused-Somatic-Hypermutation-in-CLL-IGHV",
+  },
+  {
+    name: "Gemenetzi, Katerina, et al. 'Remarkable functional constraints on the antigen receptors of CLL stereotyped subset# 2: high-throughput immunogenetic evidence.' Blood 132 (2018): 1839.",
+    link: "https://www.sciencedirect.com/science/article/pii/S0006497119378887",
+  },
+  {
+    name: "Karapetsas, Athanasios, et al. 'The homeodomain transcription factor MEIS1 triggers chemokine expression and is involved in CD8+ T‐lymphocyte infiltration in early stage ovarian cancer.' Molecular carcinogenesis 57.9 (2018): 1251-1263. ",
+    link: " https://onlinelibrary.wiley.com/doi/abs/10.1002/mc.22840",
+  },
+  {
+    name: "Markopoulos, Georgios S., et al. 'Roles of NF-κB signaling in the regulation of miRNAs impacting on inflammation in cancer.' Biomedicines 6.2 (2018): 40. ",
+    link: "https://www.mdpi.com/2227-9059/6/2/40 ",
+  },
+  {
+    name: "Markopoulos, Georgios S., et al. 'Senescence-associated microRNAs target cell cycle regulatory genes in normal human lung fibroblasts.' Experimental gerontology 96 (2017): 110-122. ",
+    link: "https://www.sciencedirect.com/science/article/pii/S053155651730493X ",
+  },
+  {
+    name: "Markopoulos, Georgios S., et al. 'A step-by-step microRNA guide to cancer development and metastasis.' Cellular Oncology 40.4 (2017): 303-339. ",
+    link: "https://link.springer.com/article/10.1007/s13402-017-0341-9 ",
+  },
+  {
+    name: "Imprialos, Konstantinos, et al. 'The microRNA 221/222 Cluster: Inaugurating a New Era in Cardiovascular Disease and Cancer?.' Current vascular pharmacology 15.1 (2017): 47-50.",
+    link: "https://www.ingentaconnect.com/contentone/ben/cvp/2017/00000015/00000001/art00010 ",
+  },
+  {
+    name: "Mizas, Charilaos, et al. 'DNA cellular automata.' Designing beauty: the art of cellular automata. Springer, Cham, 2016. 127-128. ",
+    link: "https://link.springer.com/chapter/10.1007/978-3-319-27270-2_21",
+  },
+  {
+    name: "Karapetsas, Athanasios, et al. 'Overexpression of GPC6 and TMEM132D in early stage ovarian cancer correlates with CD8+ T-lymphocyte infiltration and increased patient survival.' BioMed research international 2015 (2015). ",
+    link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4584051/ ",
+  },
+  {
+    name: "Karapetsas, Athanasios, et al. 'Novel microRNAs as putative therapeutic targets in cardiovascular diseases.' Current vascular pharmacology 13.5 (2015): 564-565. ",
+    link: "https://www.ingentaconnect.com/content/ben/cvp/2015/00000013/00000005/art00003 ",
+  },
+  {
+    name: "Sandaltzopoulos, Raphael, and Peter B. Becker. 'Beads-on-a-String on a Bead: Reconstitution and Analysis of Chromatin on a Solid Support.' Chromatin Protocols. Humana Press, New York, NY, 2015. 1-14. ",
+    link: "https://link.springer.com/protocol/10.1007%2F978-1-4939-2474-5_1 ",
+  },
+  {
+    name: "Sandaltzopoulos, Raphael, et al. 'Zmpti1c overexpression correlates with increased adaptability of maize in low irrigation conditions.' Journal of Biotechnology 208 (2015): S110. ",
+    link: "https://www.infona.pl/resource/bwmeta1.element.elsevier-c924204c-fe0e-3610-a684-f4d24b198366",
+  },
+  {
+    name: "Amanatiadis, A., et al. 'Identification and Retrieval of DNA Genomes Using Binary Image Representations Produced by Cellular Automata.' (2014). ",
+  },
+  {
+    name: "Konstantinidis, K., et al. 'Identification and retrieval of DNA genomes using binary image representations produced by cellular automata.' 2014 IEEE International Conference on Imaging Systems and Techniques (IST) Proceedings. IEEE, 2014. ",
+    link: "https://ieeexplore.ieee.org/abstract/document/6958460. ",
+  },
+  {
+    name: "Georgiadis, Andreas, et al. 'Melt-curve-multiplex-haplotype-specific-PCR, a valuable tool for biological studies: Application in congeneric species discrimination assay.' Biochemical Systematics and Ecology 56 (2014): 271-277. ",
+    link: "https://www.sciencedirect.com/science/article/pii/S0305197814002051 ",
+  },
+  {
+    name: " Giannakakis, Antonis, et al. 'Overexpression of SMARCE1 is associated with CD8+ T-cell infiltration in early stage ovarian cancer.' The international journal of biochemistry & cell biology 53 (2014): 389-398.",
+    link: " https://www.sciencedirect.com/science/article/pii/S1357272514001897",
+  },
+  {
+    name: "Karafyllidis, Ioannis G., et al. 'Modeling the Effect of Tissue Displacement during Avascular Tumor Growth on Tumor Progression.' Asian Journal of Fuzzy and Applied Mathematics (ISSN: 2321–564X) 2.01 (2014). ",
+  },
+  {
+    name: " Konstantinidis, K., et al. 'Identification and retrieval of DNA genomes using binary image representations produced by cellular automata.' 2014 IEEE International Conference on Imaging Systems and Techniques (IST) Proceedings. IEEE, 2014.",
+    link: "http://hephaestus.nup.ac.cy/handle/11728/10215 ",
+  },
+  {
+    name: "Matthaiou, Efthymia-Iliana, et al. 'Shikonin-loaded antibody-armed nanoparticles for targeted therapy of ovarian cancer.' International journal of nanomedicine 9 (2014): 1855. ",
+    link: " https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3998853/",
+  },
+  {
+    name: "Dangaj, Denarda, et al. 'Novel recombinant human b7-h4 antibodies overcome tumoral immune escape to potentiate T-cell antitumor responses.' Cancer research 73.15 (2013): 4820-4829. ",
+    link: " https://cancerres.aacrjournals.org/content/73/15/4820.short ",
+  },
+  {
+    name: "Lanitis, Evripidis, et al. 'Chimeric antigen receptor T Cells with dissociated signaling domains exhibit focused antitumor activity with reduced potential for toxicity in vivo.' Cancer immunology research 1.1 (2013): 43-53. ",
+    link: " https://cancerimmunolres.aacrjournals.org/content/1/1/43.short ",
+  },
+  {
+    name: "SANDALTZOPOULOS, R., and PB BECKER. 'Antirepression, Potentiation and Activation.' Mechanisms of Transcription 11 (2012): 291.",
+  },
+  {
+    name: "Lanitis, Evripidis, et al. 'Primary human ovarian epithelial cancer cells broadly express HER2 at immunologically-detectable levels.' Plos one 7.11 (2012): e49829. ",
+    link: "https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.876.8002&rep=rep1&type=pdf ",
+  },
+  {
+    name: "Lanitis, Evripidis, et al. 'Primary human ovarian epithelial cancer cells broadly express HER2 at immunologically-detectable levels.' Plos one 7.11 (2012): e49829. ",
+    link: "https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0049829",
+  },
+  {
+    name: "Dangaj, Denarda, et al. 'A novel anti-B7-H4 human recombinant antibody overcomes TAM-mediated T-cell inhibition (46.43).' (2012): 46-43. ",
+    link: " https://www.jimmunol.org/content/188/1_Supplement/46.43.short",
+  },
+  {
+    name: "Lanitis, Evripidis, et al. 'Development of a CAR T Cell Platform with Dissociated Signalling Domains for Selective Reactivity Against Ovarian Cancer.' MOLECULAR THERAPY. Vol. 20. 75 VARICK ST, 9TH FLR, NEW YORK, NY 10013-1917 USA: NATURE PUBLISHING GROUP, 2012. ",
+  },
+  {
+    name: "Lanitis, Evripidis, et al. 'Redirected antitumor activity of primary human lymphocytes transduced with a fully human anti-mesothelin chimeric receptor.' Molecular Therapy 20.3 (2012): 633-643. ",
+    link: "https://www.sciencedirect.com/science/article/pii/S1525001616305214 ",
+  },
+  {
+    name: " Dangaj, Denarda, et al. 'Mannose receptor (MR) engagement by mesothelin GPI anchor polarizes tumor-associated macrophages and is blocked by anti-MR human recombinant antibody.' Plos one 6.12 (2011): e28386.",
+    link: " https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0028386",
+  },
+  {
+    name: " Karapetsas, Athanasios, et al. 'Biochemical and molecular analysis of the interaction between ERK2 MAP kinase and hypoxia inducible factor-1α.' The international journal of biochemistry & cell biology 43.11 (2011): 1582-1590. ",
+    link: " https://www.sciencedirect.com/science/article/pii/S1357272511001993",
+  },
+  {
+    name: " Sasaroli, Dimitra, et al. 'Novel surface targets and serum biomarkers from the ovarian cancer vasculature.' Cancer biology & therapy 12.3 (2011): 169-180.",
+    link: "https://www.tandfonline.com/doi/full/10.4161/cbt.12.3.16260",
+  },
+  {
+    name: "Dangaj, Denarda, et al. 'Mannose receptor regulates macrophage polarization in the tumor microenvironment (66.31).' (2011): 66-31. ",
+    link: "https://www.jimmunol.org/content/186/1_Supplement/66.31.short",
+  },
+  {
+    name: " Li, Chunsheng, et al. 'Tumor vascular biomarkers: New opportunities for cancer diagnostics 1.' Cancer Biomarkers 8.4-5 (2011): 253-271.",
+    link: " https://content.iospress.com/articles/cancer-biomarkers/cbm00217 ",
+  },
+  {
+    name: " Thodi, Georgia, et al. 'Screening of the DNA mismatch repair genes MLH1, MSH2 and MSH6 in a Greek cohort of Lynch syndrome suspected families.' BMC cancer 10.1 (2010): 1-12. ",
+    link: " https://link.springer.com/article/10.1186/1471-2407-10-544 ",
+  },
+  {
+    name: " Fostira, Florentia, et al. 'Mutational spectrum of APC and genotype-phenotype correlations in Greek FAP patients.' BMC cancer 10.1 (2010): 1-8. ",
+    link: " https://link.springer.com/article/10.1186/1471-2407-10-389 ",
+  },
+  {
+    name: " Lanitis, Evripidis, et al. 'Ovarian Cancer Cells Ubiquitously Express HER-2 and can be Distinguished from Normal Ovary by Genetically Redirected T Cells.' JOURNAL OF IMMUNOTHERAPY. Vol. 33. No. 8. 530 WALNUT ST, PHILADELPHIA, PA 19106-3621 USA: LIPPINCOTT WILLIAMS & WILKINS, 2010.",
+  },
+  {
+    name: " Lanitis, Evripidis, et al. 'HER-2 is expressed by all ovarian cancer cells and invariably recognized by genetically engineered HER-2 specific T cells.' (2010): 4775-4775. ",
+    link: " https://cancerres.aacrjournals.org/content/70/8_Supplement/4775.short",
+  },
+  {
+    name: " Dangaj, Denarda, et al. 'Tumor-released GPI-anchored proteins bind to mannose receptor and polarize tumor-infiltrating macrophages.' (2010): 3809-3809.",
+    link: " https://cancerres.aacrjournals.org/content/70/8_Supplement/3809.short ",
+  },
+  {
+    name: " Kalogeropoulou, Margarita, et al. 'TAF4b and Jun/activating protein-1 collaborate to regulate the expression of integrin α6 and cancer cell migration properties.' Molecular Cancer Research 8.4 (2010): 554-568.",
+    link: " https://mcr.aacrjournals.org/content/8/4/554.short ",
+  },
+  {
+    name: " Tsika, Elpida, et al. 'Distinct region-specific α-synuclein oligomers in A53T transgenic mice: implications for neurodegeneration.' Journal of Neuroscience 30.9 (2010): 3409-3418. ",
+    link: " https://www.jneurosci.org/content/30/9/3409.short ",
+  },
+  {
+    name: " Dangaj, D., et al. 'In vivo blocking of CA125/mesothelin-dependent cell adhesion prevents ovarian cancer peritoneal metastasis.' GYNECOLOGIC ONCOLOGY. Vol. 116. No. 3. 525 B ST, STE 1900, SAN DIEGO, CA 92101-4495 USA: ACADEMIC PRESS INC ELSEVIER SCIENCE, 2010. ",
+  },
+  {
+    name: " Karapetsas, Athanasios, et al. 'Rapid detection and identification of probiotic Lactobacillus casei ATCC 393 by multiplex PCR.' Journal of molecular microbiology and biotechnology 18.3 (2010): 156-161. ",
+    link: " https://www.karger.com/Article/Abstract/308518 ",
+  },
+  {
+    name: " Galanis, Alex, Athanasios Karapetsas, and Raphael Sandaltzopoulos. 'Metal-induced carcinogenesis, oxidative stress and hypoxia signalling.' Mutation research/Genetic toxicology and environmental mutagenesis 674.1-2 (2009): 31-35.",
+    link: " https://www.sciencedirect.com/science/article/pii/S1383571808003057 ",
+  },
+  {
+    name: "Sandaltzopoulos, Raphael, and Peter B. Becker. 'Analysis of reconstituted chromatin using a solid-phase approach.' Chromatin Protocols. Humana Press, 2009. 11-25. ",
+    link: "https://link.springer.com/protocol/10.1007/978-1-59745-190-1_2 ",
+  },
+  {
+    name: "Apostolidis, Apostolos P., et al. 'Reliable and rapid discrimination of congeneric species by mtDNA SNP analysis by multiplex PCR: application on three Trachurus and two Mullus fish species as model cases.' Hydrobiologia 614.1 (2008): 401-404.",
+    link: " https://link.springer.com/article/10.1007/s10750-008-9528-4",
+  },
+  {
+    name: "Galanis, Alex, et al. 'Reactive oxygen species and HIF-1 signalling in cancer.' Cancer letters 266.1 (2008): 12-20. ",
+    link: " https://www.sciencedirect.com/science/article/pii/S0304383508001420",
+  },
+  {
+    name: "Fostira, F., et al. 'Mutations in APC gene and constitutive activation of Wnt-pathway: PP3C-12.' The Febs Journal 275 (2008). ",
+  },
+  {
+    name: " Thodi, G., et al. 'Mutations in MLH1 and MSH2 genes and impairment of the DNA repair pathway: PP3C-41.' The Febs Journal 275 (2008).",
+  },
+  {
+    name: " Zhang, Lin, et al. 'Genomic and epigenetic alterations deregulate microRNA expression in human epithelial ovarian cancer.' Proceedings of the National Academy of Sciences 105.19 (2008): 7004-7009.",
+    link: " https://www.pnas.org/content/105/19/7004.short",
+  },
+  {
+    name: "Mizas, Ch, et al. 'Reconstruction of DNA sequences using genetic algorithms and cellular automata: Towards mutation prediction?.' Biosystems 92.1 (2008): 61-68. ",
+    link: "https://www.sciencedirect.com/science/article/pii/S0303264707001815",
+  },
+  {
+    name: "Antoniou, P. E., et al. 'Investigating the potential of polymer gel dosimetry for interventional radiology: first results.' Physics in Medicine & Biology 53.8 (2008): N127. ",
+    link: " https://iopscience.iop.org/article/10.1088/0031-9155/53/8/N02/meta",
+  },
+  {
+    name: "Giannakakis, Antonis, et al. 'miR-210 links hypoxia with cell cycle regulation and is deleted in human epithelial ovarian cancer.' Cancer biology & therapy 7.2 (2008): 255-264. ",
+    link: " https://www.tandfonline.com/doi/abs/10.4161/cbt.7.2.5297",
+  },
+  {
+    name: "Kalogeropoulou, M., et al. 'Specific interactions of TAFs with AP-1 components in the regulation of target genes during RAS transformation.' Febs Journal. Vol. 275. 2008. ",
+  },
+  {
+    name: "Pappa, A., et al. 'Sulfur-containing compounds in protecting against oxidant-mediated lung diseases.' Current medicinal chemistry 14.24 (2007): 2590-2596. ",
+    link: "https://www.ingentaconnect.com/content/ben/cmc/2007/00000014/00000024/art00008 ",
+  },
+  {
+    name: "Fostira, Florentia, et al. 'Hereditary cancer syndromes.' J BUON 12.Suppl 1 (2007): S13-22. ",
+    link: " https://www.researchgate.net/profile/Georgia-Thodi/publication/5910627_Hereditary_cancer_syndromes/links/09e4150588dc0198ca000000/Hereditary-cancer-syndromes.pdf ",
+  },
+  {
+    name: "Apostolidis, Apostolos P., et al. 'Rapid identification of Salmo trutta lineages by multiplex PCR utilizing primers tailored to discriminate single nucleotide polymorphisms (SNPs) of the mitochondrial control region.' Conservation Genetics 8.5 (2007): 1025-1028. ",
+    link: "  https://link.springer.com/content/pdf/10.1007/s10592-006-9239-1.pdf",
+  },
+  {
+    name: "Giannakakis, Antonis, et al. 'miRNA genetic alterations in human cancers.' Expert opinion on biological therapy 7.9 (2007): 1375-1386.",
+    link: " https://www.tandfonline.com/doi/abs/10.1517/14712598.7.9.1375",
+  },
+  {
+    name: "Buckanovich, Ronald J., et al. 'Tumor vascular proteins as biomarkers in ovarian cancer.' Journal of clinical oncology 25.7 (2007): 852-861. ",
+    link: "https://www.researchgate.net/profile/Lance-Liotta/publication/6478200_Tumor_Vascular_Proteins_As_Biomarkers_in_Ovarian_Cancer/links/00b7d520a7c985f1b7000000/Tumor-Vascular-Proteins-As-Biomarkers-in-Ovarian-Cancer.pdf ",
+  },
+  {
+    name: "Sirakoulis, G. Ch, et al. 'An algorithm for the study of DNA sequence evolution based on the genetic code.' BioSystems 77.1-3 (2004): 11-23. ",
+    link: "https://www.sciencedirect.com/science/article/pii/S0303264704000322 ",
+  },
+];
