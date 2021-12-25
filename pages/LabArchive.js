65b84175f48037b42e62022bddf90a10606f4682
George Tsiftsis <gtsiff@gmail.com>
2021-12-25 23:18:25 +0200

    Create phd holders

--- pages/LabArchive.js
+++ pages/LabArchive.js
@@ -1,54 +1,10 @@
-import Year from "../components/Year";
-
+import PhdHolders from "../components/PhdHolders";
+import MastersHolders from "../components/MasterHolders";
 function LabArchive() {
   return (
-    <div className="flex flex-col p-4 max-4-xl items-center justify-center font-manrope">
-      <h1 className="text-5xl underline  text-center py-4 ">Αρχείο</h1>
-      <p className="py-2 text-center">
-        Παρακάτω μπορείτε να βρείτε τις πτυχιακές διατριβές των εργαστηρίων μας
-        ταξινομημενες ανα χρονολογία
-      </p>
-      <h1 className=" text-xl p-2 font-semibold"> Διδακτορικές Διατριβές </h1>
-      <ul className="flex flex-col items-center justify-center">
-        <Year
-          title="Σεμιλιέτωφ Κατερίνα, Ανάπτυξη και αξιολόγηση συστήματος ανθρωποποιημένων ποντικών ως προκλινικό μοντέλο καινοτόμων αντικαρκινικών ανοσοθεραπειών"
-          link="https://www.didaktorika.gr/eadd/handle/10442/46049"
-          year="2019"
-          state={true}
-        />
-        <Year
-          title="Τοκαμάνη Μαρία, Μελέτη miRNA που εμπλέκονται σε σηματοδοτικά μονοπάτια στον καρκίνο του πνεύμονα"
-          link="https://thesis.ekt.gr/thesisBookReader/id/43980?lang=el#page/1/mode/2up"
-          year="2018"
-          state={true}
-        />
-        <Year
-          title=" Ματθαίου Ευθυμία-Ηλιάνα, Νανοθεραπευτικές προσεγγίσεις σε επιθηλιακούς καρκίνους"
-          link="https://thesis.ekt.gr/thesisBookReader/id/40575?lang=el#page/1/mode/2up"
-          year="2017"
-          state={true}
-        />
-        <Year
-          title=" Καραπέτσας Θανάσης,  Μελέτη μοριακών δεικτών στον καρκίνο των ωοθηκών"
-          link="https://thesis.ekt.gr/thesisBookReader/id/36871#page/2/mode/2up"
-          year="2014"
-          state={true}
-        />
-        <Year
-          title="Ευρυπίδης Λανίτης, Ανάπτυξη και αξιολόγηση πειραματικού συθστήματος κυτταριθκής ανοσοθεραπείας του καρκίνου των ωοθηκών"
-          link="https://thesis.ekt.gr/thesisBookReader/id/36871#page/2/mode/2up"
-          year="2013"
-          state={true}
-          title2="Ντενάρντα Ντανγκαη Ο ρόλος των μεμβρανικών γλυκοπρωτεϊνών στον καρκίνο των ωοθηκών"
-          link2="https://thesis.ekt.gr/thesisBookReader/id/28401?lang=el#page/1/mode/2up"
-        />
-        <Year
-          title="Ευρυπίδης Λανίτης, Ανάπτυξη και αξιολόγηση πειραματικού συθστήματος κυτταριθκής ανοσοθεραπείας του καρκίνου των ωοθηκών"
-          link="https://thesis.ekt.gr/thesisBookReader/id/22723?lang=el#page/1/mode/2up"
-          year="2007"
-          state={true}
-        />
-      </ul>
+    <div className="flex flex-col items-center justify-center">
+      <PhdHolders />
+      <MastersHolders />
     </div>
   );
 }
