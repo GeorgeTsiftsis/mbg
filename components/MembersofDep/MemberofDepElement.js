7ad49a4689396fb49fa9b9718ffd445e79ab86a8
George Tsiftsis <gtsiff@gmail.com>
2021-12-31 00:59:05 +0200

    add prop color

--- components/MembersofDep/MemberofDepElement.js
+++ components/MembersofDep/MemberofDepElement.js
@@ -1,6 +1,9 @@
-function MemberofDepElement({ membername, role, img, sector, link }) {
+function MemberofDepElement({ membername, role, img, sector, link, color }) {
   return (
-    <div className="flex font-manrope  flex-col w-full  sm:w-auto p-4 m-6 border-myblue border-2 items-center rounded-lg bg-myfooter">
+    <div
+      style={{ background: color }}
+      className="flex font-manrope  flex-col w-full  sm:w-auto p-4 m-6 border-myblue border-2 items-center rounded-lg bg-myfooter"
+    >
       <img className="rounded-t-lg w-72" src={img} alt="photoOfMember" />
       <h1 className="my-2 text-white">{membername} </h1>
       <p className="text-white break-all w-52 text-center "> {role}</p>
