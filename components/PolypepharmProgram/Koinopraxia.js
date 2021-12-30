f697ffcc9cd20ebf6cb4da9601ca0fa13c01aa0b
George Tsiftsis <gtsiff@gmail.com>
2021-12-31 00:39:04 +0200

    add Polyppharm programm page

---
+++ components/PolypepharmProgram/Koinopraxia.js
@@ -0,0 +1,5 @@
+function Koinopraxia() {
+  return <div className=" items-center flex flex-col  "></div>;
+}
+
+export default Koinopraxia;
---
+++ components/PolypepharmProgram/index.js
@@ -0,0 +1,37 @@
+import Koinopraxia from "./Koinopraxia";
+
+function PolypepharmProgram() {
+  return (
+    <div className="font-manrope p-4 py-8 max-w-5xl ">
+      <h1 className=" text-lg pb-2 ">
+        ΤΙΤΛΟΣ: Βελτιωμένα γενόσημα πολυπεπτιδικά φάρμακα με καινοτόμες
+        φαρμακοτεχνικές μορφές.
+      </h1>
+      <h1>
+        &quot;Improved generic polypeptide drugs with innovative pharmaceutical
+        forms&quot;
+      </h1>
+      <h1>ΑΚΡΩΝΥΜΙΟ: POLYPEPPHARM</h1>
+      <h1>
+        ΚΩΔΙΚΟΣ ΕΡΓΟΥ: <strong className="font-bold">Τ2ΕΔΚ-00996 </strong>
+      </h1>
+      <h1>MIS: 5033599</h1>
+      <h1 id="Polypeppharm">
+        Πρόγραμμα: ΔΡΑΣΗ ΕΘΝΙΚΗΣ ΕΜΒΕΛΕΙΑΣ: «ΕΡΕΥΝΩ-ΔΗΜΙΟΥΡΓΩ-ΚΑΙΝΟΤΟΜΩ Β’
+        ΚΥΚΛΟΣ», «ΑΝΤΑΓΩΝΙΣΤΙΚΟΤΗΤΑ, ΕΠΙΧΕΙΡΗΜΑΤΙΚΟΤΗΤΑ & ΚΑΙΝΟΤΟΜΙΑ» (ΕΠΑνΕΚ)
+      </h1>
+      <p className="text-gray-400 pt-4 text-justify">
+        Το έργο POLYPEPPHARM έχει ως στόχο την ανάπτυξη φαρμακευτικών
+        πολυπεπτιδίων μέσω της έκφρασής τους σε ένα ευκαρυωτικό σύστημα σε
+        αντιδιαστολή προς τα βακτηριακά συστήματα που χρησιμοποιούνται σήμερα.
+        Τα πολυπεπτίδια παράγονται σε εκκρινόμενη μορφή ώστε να διαθέτουν
+        πολυσακχαριτικές ομάδες, γεγονός που τους προσδίδει μεγαλύτερη δομική
+        σταθερότητα, βελτιωμένη λειτουργικότητα και μεγαλύτερη διάρκεια
+        παραμονής στην κυκλοφορία του ασθενούς.
+      </p>
+      <Koinopraxia />
+    </div>
+  );
+}
+
+export default PolypepharmProgram;
