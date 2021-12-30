324c4d134a76919b255bb6e5fa9b6ac76272e6c9
George Tsiftsis <gtsiff@gmail.com>
2021-12-31 01:03:22 +0200

    Layout for navbaer *need to fix

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,28 @@
 import Navbar from "./navbar/index";
+import { useRouter } from "next/router";
+import SecondNavbar from "./navbar/SecondNavbar";
+
 import Footer from "./footer";
 
 function Layout({ children }) {
+  const router = useRouter();
+  // let navbar;
+  // if (router.asPath == "/Polypeppharm") {
+  //   navbar = <SecondNavbar />;
+  // } else if (router.asPath == "/Polypeppharm/EreunitikesOmades") {
+  //   navbar = <SecondNavbar />;
+  // } else if (router.asPath == "/Polypeppharm/Sinergates") {
+  //   navbar = <SecondNavbar />;
+  // } else {
+  //   <Navbar />;
+  // }
   return (
     <>
-      <Navbar />
+      {router.asPath == "/Polypeppharm" && "/Polypeppharm/Sinergates" ? (
+        <SecondNavbar />
+      ) : (
+        <Navbar />
+      )}
       <main>{children}</main>
       <Footer />
     </>
