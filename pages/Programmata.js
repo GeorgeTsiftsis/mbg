599948461fce0eab2eb91909b6062349430fdff3
George Tsiftsis <gtsiff@gmail.com>
2021-12-31 00:58:52 +0200

    change styling

--- pages/Programmata.js
+++ pages/Programmata.js
@@ -5,13 +5,13 @@ function Programmata() {
     <div className="font-manrope flex flex-col justify-center items-center">
       <h1 className="text-center text-5xl underline py-4">Προγράμματα</h1>
       <h1 className=" p-2 text-3xl"> Sandaltzopoulos Laboratory</h1>
-      <h1 className=" text-2xl text-left">Ενεργά ερευνητικά προγράμματα</h1>
-      <img className="w-1/4 rounded-xl" src="./lampes.png" />
-      <ul className="flex flex-col text-blue-400 items-center justify-center">
-        <li className=" hover:text-blue-300 p-2">
+      <h1 className=" text-xl text-left">Ενεργά ερευνητικά προγράμματα</h1>
+      <img className="w-3/4 max-w-4xl py-2 rounded-xl" src="./lampes.png" />
+      <ul className="flex flex-col p-4 text-justify space-y-2 text-blue-400 items-center justify-center">
+        <li className=" hover:text-blue-300 ">
           <Link passHref href="/Polypeppharm">
             <a target="_blank" rel="noopener noreferrer">
-              PolypepPharm: Βελτιωμένα γενόσημα πολυπεπτιδικά φάρμακα με
+              1. PolypepPharm: Βελτιωμένα γενόσημα πολυπεπτιδικά φάρμακα με
               καινοτόμες φαρμακοτεχνικές μορφές.
             </a>
           </Link>
@@ -23,8 +23,8 @@ function Programmata() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            FishMID: Έλεγχος αυθεντικότητας και νοθείας αλιευμάτων της ελληνικής
-            αγοράς με μοριακή ταυτοποίηση.
+            2. FishMID: Έλεγχος αυθεντικότητας και νοθείας αλιευμάτων της
+            ελληνικής αγοράς με μοριακή ταυτοποίηση.
           </a>
         </li>
       </ul>
