ebb6be76df471528384f952f8ae9e2fe8b2ca6ba
George Tsiftsis <gtsiff@gmail.com>
2022-01-01 19:42:37 +0200

    add props

--- components/navbar/Members.js
+++ components/navbar/Members.js
@@ -13,24 +13,22 @@ function Members(props) {
     // },
   ];
   return (
-    <div className="  ">
-      <ul className="font-fancy opacity-70 bg-white rounded-lg px-2">
-        {memberrs.map(({ nameofLab, href }) => (
-          <li key={href} className=" hover:text-blue-400   z-10 md:text-left">
-            <Link href={href}>
-              <a
-                className={`text-xl underline  ${
-                  router.asPath === href ? "text-blue-400" : ""
-                }`}
-                passhref={href}
-              >
-                {nameofLab}
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="font-fancy opacity-70 bg-white rounded-lg px-2">
+      {memberrs.map(({ nameofLab, href }) => (
+        <li key={href} className=" hover:text-blue-400   z-10 md:text-left">
+          <Link href={href}>
+            <a
+              className={`text-xl underline  ${
+                router.asPath === href ? "text-blue-400" : ""
+              }`}
+              passhref={href}
+            >
+              {nameofLab}
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 }
 
