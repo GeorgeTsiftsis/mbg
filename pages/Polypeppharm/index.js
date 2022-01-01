3f3a3aac9fef920790a8f3e51866088ec21defc0
George Tsiftsis <gtsiff@gmail.com>
2022-01-01 19:45:48 +0200

    import koinopraksia

--- pages/Polypeppharm/index.js
+++ pages/Polypeppharm/index.js
@@ -1,11 +1,13 @@
 import Carousel, { images2 } from "../../components/Carousel/index";
 import PolypepharmProgram from "../../components/PolypepharmProgram";
 import EnotitesErgasias from "../../components/EnotitesErgasias";
+import Koinopraxia from "../../components/PolypepharmProgram/Koinopraxia";
 function Polypeppharm() {
   return (
     <div className="flex flex-col justify-items items-center">
       <Carousel images={images2} />
       <PolypepharmProgram />
+      <Koinopraxia />
       <EnotitesErgasias />
     </div>
   );
