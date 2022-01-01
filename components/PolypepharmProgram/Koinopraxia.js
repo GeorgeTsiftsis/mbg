c53a14e023458a4f0477fafba461f2fd0f9dec29
George Tsiftsis <gtsiff@gmail.com>
2022-01-01 19:46:56 +0200

    ADD koinoprkasia elements

--- components/PolypepharmProgram/Koinopraxia.js
+++ components/PolypepharmProgram/Koinopraxia.js
@@ -1,5 +1,44 @@
+import DraseisChild from "../Draseis/DraseisChild";
 function Koinopraxia() {
-  return <div className=" items-center flex flex-col  "></div>;
+  return (
+    <div id="Koinopraxia" className="bg-mygray w-full h-auto  pb-6">
+      <h1 className="text-center pt-20 font-fancy text-5xl  text-white ">
+        Κοινοπραξία
+      </h1>
+      <h1 className="text-white text-center font-fancy  text-lg  py-8">
+        Η κοινοπραξία μας αποτελείται από 12 συνεργάτες από 4 φορείς (3
+        ακαδημαϊκούς φορείς και 1 επιχείρηση) οι οποίοι καλύπτουν ένα εύρος
+        ειδικοτήτων.
+      </h1>
+      <div className="flex items-center justify-center w-full border-t-4 pt-12  flex-wrap ">
+        <DraseisChild
+          title2="Τμήμα Μοριακής Βιολογίας & Γενετικής - ΔΠΘ"
+          button="Πατήστε εδώ"
+          img="/mbg-lo.png"
+          link2="https://www.mbg.duth.gr/index.php/"
+        />
+
+        <DraseisChild
+          title2="Τμήμα Ιατρικής Πανεπηστήμιο Πατρών"
+          button="Πατήστε εδώ"
+          img="/iatriki-lo.png"
+          link2="https://www.med.upatras.gr"
+        />
+        <DraseisChild
+          title2="Τμήμα Φραμκευτικης Πανεπηστήμιο Πατρών"
+          button="Πατήστε εδώ"
+          img="/farmakeutiki-lo.png"
+          link2="http://www.pharmacy.upatras.gr/index.php/el"
+        />
+        <DraseisChild
+          title2="Verisified"
+          button="Πατήστε εδώ"
+          img="/Verisfield-lo.png"
+          link2="https://www.verisfield.gr"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Koinopraxia;
