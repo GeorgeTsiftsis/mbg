3dc796673cbea331b2ab4eb781d17e0b00ced893
George Tsiftsis <gtsiff@gmail.com>
2022-01-01 19:42:19 +0200

    change draseischild to turn into functional comp

--- components/Draseis/DraseisChild.js
+++ components/Draseis/DraseisChild.js
@@ -1,15 +1,35 @@
 import Link from "next/link";
-function DraseisChild({ title, desc, button, img, link }) {
+function DraseisChild({ title, title2, desc, button, img, link, link2 }) {
   return (
     <div className="font-fancy flex flex-col items-center w-62  p-2">
       <img src={img} alt={img + "image"} />
-      <h1 className="text-white pb-2  text-center text-xl">{title}</h1>
-      <h1 className="text-white text-center w-2/3 ">{desc}</h1>
-      <Link passHref={true} href={link}>
-        <button className="p-2 m-8 border-blue-200 bg-mybrown border-2 text-white rounded-xl">
-          {button}
-        </button>
-      </Link>
+      {title && (
+        <h1 className="text-white pb-2  text-center text-xl">{title}</h1>
+      )}
+      {title2 && (
+        <h1
+          style={{ maxWidth: "20rem", height: "70px" }}
+          className="text-white py-8   text-center text-xl"
+        >
+          {title2}
+        </h1>
+      )}
+
+      {desc && <h1 className="text-white text-center w-2/3 ">{desc}</h1>}
+      {link && (
+        <Link passHref={true} href={link}>
+          <button className="p-2 m-8 border-blue-200 bg-mybrown border-2 text-white rounded-xl">
+            {button}
+          </button>
+        </Link>
+      )}
+      {link2 && (
+        <a target="_blank" rel="noopener noreferrer" href={link2}>
+          <button className="p-2 m-8 border-blue-200 bg-mybrown border-2 text-white rounded-xl">
+            {button}
+          </button>
+        </a>
+      )}
       <span
         style={{ width: "300px" }}
         className="border-b-2 sm:border-b-0 "
