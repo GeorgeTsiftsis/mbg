d5a072d6dcce6f40199f425e54a5a282ac4cee53
George Tsiftsis <gtsiff@gmail.com>
2022-01-05 15:47:48 +0200

    change favicon

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Manrope&family=Roboto:wght@300&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <body>
