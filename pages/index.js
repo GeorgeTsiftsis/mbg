a6522d5384cb5c5cccabe1a1660cdba397cfae40
George Tsiftsis <gtsiff@gmail.com>
2022-01-05 15:19:58 +0200

    Next Seo for the home land page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Draseis from "../components/Draseis";
 import Carousel from "../components/Carousel";
 import MiniInfo from "../components/MiniInfo/index";
@@ -6,19 +5,25 @@ import MembersofDep from "../components/MembersofDep";
 import EpikentroEreunas from "../components/EpikentroEreunas";
 import BottomToTop from "../components/ButtonTotop/index";
 import { images } from "../components/Carousel";
+import { NextSeo } from "next-seo";
+
 export default function Home() {
   return (
-    <div className=" flex flex-col items-center justify-center">
-      <Head>
-        <title>Theranotics MBG Duth </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Carousel images={images} />
-      <BottomToTop />
-      <MiniInfo />
-      <EpikentroEreunas />
-      <MembersofDep />
-      <Draseis title="ΔΡΑΣΕΙΣ" description=" 20+ Χρόνια Εμπειρίας" />
-    </div>
+    <>
+      <NextSeo
+        title="Theranotics MBG Duth"
+        description="Εργαστήριο Γονιδιακής Έκφρασης, Μοριακής Διάγνωσης και Σύγχρονων
+      Θεραπευτικών Μέσων του τμήματος Μοριακής Βιολογίας και Γενετικής, των
+      Σχολών Υγείας του ΔΠΘ."
+      />
+      <div className=" flex flex-col items-center justify-center">
+        <Carousel images={images} />
+        <BottomToTop />
+        <MiniInfo />
+        <EpikentroEreunas />
+        <MembersofDep />
+        <Draseis title="ΔΡΑΣΕΙΣ" description=" 20+ Χρόνια Εμπειρίας" />
+      </div>
+    </>
   );
 }
