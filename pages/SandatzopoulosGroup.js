2a375e3c8a8b524d2e45f27685dfa2e2c1774453
George Tsiftsis <gtsiff@gmail.com>
2022-01-05 15:16:42 +0200

    Add next seo

--- pages/SandatzopoulosGroup.js
+++ pages/SandatzopoulosGroup.js
@@ -1,16 +1,23 @@
 import MembersOfSandaltzopoulosGroup from "../components/MembersOfSandaltzopoulosGroup";
 import Graduates from "../components/MembersOfSandaltzopoulosGroup/Graduates";
+import { NextSeo } from "next-seo";
 function SandatzopoulosGroup() {
   return (
-    <div className=" items-center justify-items flex flex-col">
-      <h1 className="text-center font-manrope text-5xl underline py-8">
-        Μέλη Εργαστηρίου
-      </h1>
-      <div className="font-manrope flex flex-wrap items-center max-w-5xl justify-center">
-        <MembersOfSandaltzopoulosGroup />
+    <>
+      <NextSeo
+        title="Μέλη Εργαστηρίου του Theranotics MBG Duth"
+        description="Τα Μέλη του εργαστηρίου μας"
+      />
+      <div className=" items-center justify-items flex flex-col">
+        <h1 className="text-center font-manrope text-5xl underline py-8">
+          Μέλη Εργαστηρίου
+        </h1>
+        <div className="font-manrope flex flex-wrap items-center max-w-5xl justify-center">
+          <MembersOfSandaltzopoulosGroup />
+        </div>
+        <Graduates />
       </div>
-      <Graduates />
-    </div>
+    </>
   );
 }
 
