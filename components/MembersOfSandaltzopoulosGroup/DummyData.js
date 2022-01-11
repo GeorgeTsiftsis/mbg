5f0c375455fc7b14cebff8f618df9fdc3deeaede
George Tsiftsis <gtsiff@gmail.com>
2022-01-11 23:26:23 +0200

    import data

--- components/MembersOfSandaltzopoulosGroup/DummyData.js
+++ components/MembersOfSandaltzopoulosGroup/DummyData.js
@@ -28,7 +28,7 @@ export const data2row = [
 ];
 
 export const data3row = [
-  { name: "Κατερίνα Σεμιλιέτωφ", url: "linked" },
+  { name: "Κατερίνα Σεμιλιέτωφ" },
   {
     name: "Βαγγέλης Στεφανίδης",
     url: "https://www.linkedin.com/in/evangelos-stefanidis-5574a4120/",
@@ -47,4 +47,42 @@ export const mastergraduates = [
   { name: "Κανέλα Καφετζοπούλου", year: 2019 },
   { name: "Σοφία Μουράτη", year: 2019 },
   { name: "Σοφία Χρονοπούλου", year: 2020 },
+];
+
+export const bachelorgraduates1 = [
+  { name: "Γιώργος Φέλλας", year: 2004 },
+  { name: "Ευριπίδης Λανίτης", year: 2006 },
+  { name: "Denarda Dangaj", year: 2006 },
+  { name: "Θανάσης Καραπέτσας", year: 2008 },
+  { name: "Ελένη Κυρατζοπούλου", year: 2008 },
+  { name: "Καλλιόπη Αρκουδή", year: 2011 },
+  { name: "Μαρία Μακκού", year: 2012 },
+  { name: "Ευθυμία Ι. Ματθαίου", year: 2012 },
+  { name: "Αναστασία Καπάρα", year: 2012 },
+  { name: "Δημήτρης Αλατζόγλου", year: 2012 },
+];
+export const bachelorgraduates2 = [
+  { name: "Κατερίνα Σεμιλιέτωφ", year: 2012 },
+  { name: "Κατερίνα Γελαδάκη", year: 2013 },
+  { name: "Τοκαμάνη Μαρία", year: 2013 },
+  { name: "Ελένη Κυριακοπούλου", year: 2014 },
+  { name: "Χρίστος Ευαγγέλου", year: 2014 },
+  { name: "Μαρία Ασλάνη", year: 2015 },
+  { name: "Πέτρος Τσαλαγράδας", year: 2015 },
+  { name: "Ελένη Μανωλούδη", year: 2016 },
+  { name: "Ιωάννα Πανδή", year: 2016 },
+  { name: "Κανέλα Καφετζοπούλου", year: 2017 },
+  { name: "Μαρία Κορπίδου", year: 2017 },
+];
+export const bachelorgraduates3 = [
+  { name: "Ελπίδα Ζυρίδου", year: 2018 },
+  { name: "Σοφία Μουράτη", year: 2018 },
+  { name: "Χριστίνα Νιαβή", year: 2018 },
+  { name: "Ελευθερία Φίγγου", year: 2019 },
+  { name: "Λητώ Παπαμιχαήλ", year: 2019 },
+  { name: "Δήμητρα Ανατόλου", year: 2020 },
+  { name: "Άννα Γιασεμή", year: 2020 },
+  { name: "Δήμητρα Ανατόλου", year: 2020 },
+  { name: "Πέτρος Τσοκμακίδης", year: 2021 },
+  { name: "Κερασούλα Δ. Κοκκινοπλίτη", year: 2021 },
 ];
